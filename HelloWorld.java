9bd3f7d1b1ca61ca93b37f5b56c771e115e5ff2b
stemina32 <hermes.33@windowslive.com>
2019-02-10 15:29:48 -0500

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+/* Celestin Masson
+Lab 0- Push HelloWorld to github*/
+public class HelloWorld {
+  public static void main(String[] args) {
+//Prints what ever is between the quotation marks
+    System.out.println("Hell, World of 007");
+  }
+}
