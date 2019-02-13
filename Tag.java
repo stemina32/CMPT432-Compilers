cecf08c49e59e87ac2761752e14f2d9147a7fac8
stemina32 <hermes.33@windowslive.com>
2019-02-13 09:35:48 -0500

    Tag class
    Need to go back to

---
+++ Tag.java
@@ -0,0 +1,9 @@
+package lexer;
+
+public class Tag {
+	
+//declare all tags that will be using as int. Need a true/false tag
+	public final static int NUM = 256, ID = 257, True = 258, False = 259;
+	
+	
+}
