e688a65e58031d92ed170f5850b1341d27f0c70c
stemina32 <hermes.33@windowslive.com>
2019-02-13 09:39:01 -0500

    Main class Token

---
+++ token.java
@@ -0,0 +1,15 @@
+package lexer;
+
+//Main class for the lexer
+public class Token {
+	
+	
+//Declaring attribute at integer and naming it tag
+	public final int tag;
+	
+	public Token(int tag) {
+		
+		this.tag = tag;
+	}
+	
+}
